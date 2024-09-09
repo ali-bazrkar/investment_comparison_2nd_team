522ce949856cd2dd8a9fd3ad08655e71edea271a
hooman-behsaz <hoomanbehsaz@gmail.com>
2024-09-09 19:06:17 +0330

    Add files via upload

--- Project_1.py
+++ Project_1.py
@@ -1,5 +1,32 @@
-
 import yfinance as yf
+import pandas as pd
 
-data = yf.download("Gold")
-print(data)
+# Define the tickers for each asset
+tickers = {
+    'Oil': 'CL=F',            # Crude Oil WTI
+    'Gold': 'GC=F',           # Gold Futures
+    'Dollar Index': 'DX-Y.NYB',  # US Dollar Index
+    'Bitcoin': 'BTC-USD'      # Bitcoin USD
+}
+
+# Create an empty dictionary to store dataframes
+dataframes = {}
+
+# Set the start and end dates
+start_date = '2015-01-01'
+end_date = '2024-01-01'
+
+# Download the data for each asset
+for asset, ticker in tickers.items():
+    data = yf.download(ticker, start=start_date, end=end_date)
+    data['Asset'] = asset
+    dataframes[asset] = data[['Close']].rename(columns={'Close': f'{asset} Price'})
+
+# Merge the dataframes on Date
+merged_data = pd.concat(dataframes.values(), axis=1, join='inner')
+
+# Save the merged data to a CSV file
+merged_data.to_csv('merged_asset_prices.csv')
+
+# Show a preview of the data
+print(merged_data.head())
